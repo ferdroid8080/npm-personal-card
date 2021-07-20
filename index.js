faa3e7727b6cf0bbd40314da4ddbecb6f11341f6
Ferdroid8080 <andresf@ferudinato.com>
2021-07-20 16:02:45 -0500

    [fix] get card looking better in terminal

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 #!/usr/bin/env node
 
-const { white, bgGreen, green, bold, blue, yellow } = require('colorette');
+const { 
+    white, green, bold, 
+    yellow, cyan, bgBlueBright 
+} = require('colorette');
 const boxen = require('boxen');
 
-const work = white(`${blue("Ferdinania")} - Fullstack Web Developer`);
-const twitter = green("https://twitter.com/andresf_mj");
-const github = green("https://github.com/andresfmj");
-const site = green("https://ferdinania.com");
-const npx = green("npx andresfmj");
+const work = white(`${yellow("Ferudinato")} - ${cyan(`Frontend Web Developer`)}`);
+const twitter = green("https://twitter.com/ferdr0id8o8o");
+const github = green("https://github.com/ferdroid8080");
+const site = green("https://ferudinato.com");
+const npx = green("npx ferdroid8080");
 
 const newline = "\n";
-const headline = `            ${bgGreen(`${bold(white("Andres Fernando --"))} ${bold(yellow("andresfmj"))}`)}`;
+const headline = `            ${bgBlueBright(bold(`${yellow("   Ferdroid8080")} - ${white("Andres Fernando   ")}`))} 👨🏻‍💻`;
 const workLine = `${white(bold("       Work at:"))}         ${work}`;
 const twitterLine = `${white(bold("      Tweet me:"))}         ${twitter}`;
-const githubLine = `${white(bold("GitHub Profile:"))}         ${github}`;
+const githubLine = `${white(bold("        GitHub:"))}         ${github}`;
 const webLine = `${white(bold("       Website:"))}         ${site}`;
-const cardCmd = `${white(bold("Show this card:"))}         ${npx}`;
+const card = `${white(bold("     This card:"))}         ${npx} 👀`;
 
-const output = 
-    headline +
-    newline +
-    newline +
-    workLine +
-    newline +
-    twitterLine +
-    newline +
-    githubLine +
-    newline +
-    webLine +
-    newline +
-    newline +
-    cardCmd;
+const output = `${headline}
+${newline}
+${workLine}
+${twitterLine}
+${githubLine}
+${webLine}
+${newline}
+${card}
+`;
+
 
 console.log(boxen(output, { 
     padding: 1,
     borderColor: 'green',
     borderStyle: 'round',
-    margin: 1
+    margin: 0.6
 }));
